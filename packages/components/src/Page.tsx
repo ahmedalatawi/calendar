967ec0fcb0eeaa57a6f203e8df22978c87f0d2ad
Ahmed Alatawi <aalat001@gmail.com>
2023-12-14 22:47:10 -0500

    WIP

--- packages/components/src/Page.tsx
+++ packages/components/src/Page.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import Header from './Header';
-import './styles/page.scss';
+import Header from "./Header";
+import "./styles/page.scss";
 
 const Page = () => {
   const [user, setUser] = React.useState<{ name: string }>();
@@ -10,22 +10,22 @@ const Page = () => {
     <article>
       <Header
         user={user}
-        onLogin={() => setUser({ name: 'Jane Doe' })}
+        onLogin={() => setUser({ name: "Jane Doe" })}
         onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: 'Jane Doe' })}
+        onCreateAccount={() => setUser({ name: "Jane Doe" })}
       />
 
       <section className="storybook-page">
-        <h2>Pages in Storybook</h2>
+        <h2>Pages in Storybook ...</h2>
         <p>
-          We recommend building UIs with a{' '}
+          We recommend building UIs with a{" "}
           <a
             href="https://componentdriven.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             <strong>component-driven</strong>
-          </a>{' '}
+          </a>{" "}
           process starting with atomic components and ending with pages.
         </p>
         <p>
@@ -44,7 +44,7 @@ const Page = () => {
           </li>
         </ul>
         <p>
-          Get a guided tutorial on component-driven development at{' '}
+          Get a guided tutorial on component-driven development at{" "}
           <a
             href="https://storybook.js.org/tutorials/"
             target="_blank"
@@ -52,7 +52,7 @@ const Page = () => {
           >
             Storybook tutorials
           </a>
-          . Read more in the{' '}
+          . Read more in the{" "}
           <a
             href="https://storybook.js.org/docs"
             target="_blank"
@@ -64,7 +64,7 @@ const Page = () => {
         </p>
         <div className="tip-wrapper">
           <span className="tip">Tip</span> Adjust the width of the canvas with
-          the{' '}
+          the{" "}
           <svg
             width="10"
             height="10"
