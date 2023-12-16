493abaafda5c4adee559aea0b627ccf5c1affdb0
Ahmed Alatawi <aalat001@gmail.com>
2023-12-16 14:41:29 -0500

    Updated TestComponent

--- packages/components/src/TestComponent/TestComponent.tsx
+++ packages/components/src/TestComponent/TestComponent.tsx
@@ -7,7 +7,7 @@ import './TestComponent.scss'
 const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
   <div data-testid='test-component' className={`test-component test-component-${theme}`}>
     <h1 className='heading'>I am the test component</h1>
-    <h2>Made with love by Harvey</h2>
+    <h2>Made with love by Ahmed</h2>
   </div>
 )
 
