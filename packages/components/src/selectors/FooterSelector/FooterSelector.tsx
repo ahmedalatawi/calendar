061db69fca1256b4b4630f40334b9584036c2541
Ahmed Alatawi <aalat001@gmail.com>
2023-12-30 14:23:51 -0500

    Created Header and Footer components

---
+++ packages/components/src/selectors/FooterSelector/FooterSelector.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function FooterSelector() {
+  return <div>Footer Selector</div>
+}
+
+export default FooterSelector
---
+++ packages/components/src/selectors/HeaderSelector/HeaderSelector.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function HeaderSelector() {
+  return <div>Header Selector</div>
+}
+
+export default HeaderSelector
