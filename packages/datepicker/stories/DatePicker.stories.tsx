f938678951b6ddd08a7ac614bf8ccbe183097561
Ahmed Alatawi <aalat001@gmail.com>
2024-03-10 16:34:48 -0400

    Fixed issue with dayjs and storybook

--- packages/datepicker/stories/DatePicker.stories.tsx
+++ packages/datepicker/stories/DatePicker.stories.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import DatePicker from '../src/picker/DatePicker'
 
+import dayjs from 'dayjs'
+import weekday from 'dayjs/plugin/weekday'
+import localeData from 'dayjs/plugin/localeData'
+
+dayjs.extend(weekday)
+dayjs.extend(localeData)
+
 export default {
   title: 'Example/DatePicker',
   component: DatePicker,
