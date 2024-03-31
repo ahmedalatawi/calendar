398683e9de9cc36530d856ea7df0fe6485698438
Ahmed Alatawi <aalat001@gmail.com>
2024-03-31 02:50:43 -0400

    Added date and close icons

---
+++ packages/components/src/icons/CloseIcon.tsx
@@ -0,0 +1,19 @@
+import React, { SVGProps } from 'react'
+
+const CloseIcon = ({ className = 'rc-icon', onClick }: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      className={className}
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      onClick={onClick}
+    >
+      <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+    </svg>
+  )
+}
+
+export default CloseIcon
